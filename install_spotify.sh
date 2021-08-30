3fb4e764e37d82675702d5566f96f0b038337ed0
Shalmon Anandas <nashalanandas@gmail.com>
2021-08-30 15:13:13 +0530

    spotfiy script done

--- install_spotify.sh
+++ install_spotify.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-yay -S spotify-adblock spicetify-cli
-
+yay -S spotify-adblock spotify spicetify-cli
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
-
 spotify
-
+^C
 spicetify backup apply
-
 git clone https://github.com/morpheusthewhite/spicetify-themes.git
-
 cd spicetify-themes
 cp -r * ~/.config/spicetify/Themes
-
+rm -r spicetify-themes
 spicetify config current_theme Ziro
 spicetify config color_scheme "Green Dark"
-
 spicetify apply
