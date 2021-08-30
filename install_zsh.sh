3e63ba59c1be3fb29ea1cbdba01c863211835f37
Shalmon Anandas <nashalanandas@gmail.com>
2021-08-30 16:12:46 +0530

    added script to install zsh setup

---
+++ install_zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -S sed zsh
+zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="jispwoso"/g' /home/shalmon/.zshrc
