b96fc3ae209e6f3d348eb10fe5c77cb1509970ee
Shalmon Anandas <nashalanandas@gmail.com>
2021-08-30 15:39:24 +0530

    droidcam install script

---
+++ install_droidcam.sh
@@ -0,0 +1,10 @@
+sudo pacman -S unzip wget gcc make nano
+cd /tmp/
+wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.8.0.zip
+unzip droidcam_latest.zip -d droidcam
+cd droidcam
+sudo ./install-client
+sudo ./install-video
+nano ~/.config/droidcam
+sudo rmmod v4l2loopback_dc
+sudo insmod /lib/modules/`uname -r`/kernel/drivers/media/video/v4l2loopback-dc.ko width=1440 height=1080
