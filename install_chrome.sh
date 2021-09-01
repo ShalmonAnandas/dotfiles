a79f0bfcb400c519321a46705ce1abc2685fc22f
Shalmon Anandas <nashalanandas@gmail.com>
2021-09-01 12:29:58 +0530

    miscellenous changes

--- install_chrome.sh
+++ install_chrome.sh
@@ -3,4 +3,4 @@ yay -S google-chrome
 yay -S libva-vdpau-driver-chromium
 vainfo
 vdpauinfo
-printf '--ignore-gpu-blocklist\n--enable-gpu-rasterization\n--enable-zero-copy\n--enable-features=VaapiVideoDecoder\n--use-gl=desktop\n--disable-features=UseOzonePlatform'  >> ~/.config/chrome-flags.conf
+printf "--ignore-gpu-blocklist\n--enable-gpu-rasterization\n--enable-zero-copy\n--enable-features=VaapiVideoDecoder\n--use-gl=desktop\n--disable-features=UseOzonePlatform"  >> ~/.config/chrome-flags.conf
