f0574cbee81415a7f81803ce4b7e3cec166b0c08
ShalmonAnandas <nashalanandas@gmail.com>
2021-09-11 12:02:43 +0530

    "changed install_zsh.sh script"

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,4 +2,4 @@
 
 sudo pacman -S sed zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="jispwoso"/g' /home/shalmon/.zshrc
+sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="duellj"/g' /home/shalmon/.zshrc
