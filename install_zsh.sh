5c81bcb862d5cb2f6fe3ff071f6609d665257bc5
ShalmonAnandas <nashalanandas@gmail.com>
2021-11-15 10:47:49 +0530

    added zshrc

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,4 +2,3 @@
 
 sudo pacman -S sed zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="duellj"/g' /home/shalmon/.zshrc
